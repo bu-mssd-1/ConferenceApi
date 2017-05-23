79471d322347c29c1abe535991ed0a519dd7422a
Rayman Aeron <raymana@microsoft.com>
2017-05-22 21:19:48 -0700

    Enum description updated

--- ConferenceWebApi/DataProviderType.cs
+++ ConferenceWebApi/DataProviderType.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace ConferenceWebApi
 {
+    [DataContract]
     public enum DataProviderType
     {
+        [EnumMember]
         Sql = 0,
+
+        [EnumMember]
         Mock = 1,
+
+        [EnumMember]
         Xml = 2
     }
 }

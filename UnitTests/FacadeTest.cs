0f6e53bf5e4e625a1255663494f3778d342624b4
Rayman Aeron <raymana@microsoft.com>
2017-05-29 02:21:42 -0700

    Facade Test (Caling a web service from a client)

---
+++ UnitTests/FacadeTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System.Diagnostics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FacadeTest
+    {
+        [TestMethod]
+        public void GetUserViaWebService()
+        {
+            var client = new RestClient(TestConstant.ServiceUrl);
+            var request = new RestRequest("user", Method.GET);
+            request.Parameters.Add(new Parameter() { Name = "id", Value = "E18A9A29-49E0-4D8D-8528-AB70AA4BA56D" });
+
+            var result = client.Execute(request);
+
+            Assert.IsNotNull(result);
+
+            Debug.WriteLine(string.Format("Get User By Id ~ Status Code : {0}, Content : {1}", result.StatusCode, result.Content));
+        }
+    }
+}
---
+++ UnitTests/TestConstant.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTests
+{
+    public static class TestConstant
+    {
+        /// <summary>
+        /// Service url for test
+        /// </summary>
+        public static string ServiceUrl
+        {
+            get
+            {
+                return "http://localhost:58997/api/";
+            }
+        }
+    }
+}
